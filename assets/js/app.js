9fa02c7d6bc284e464a37ce1c3f0dd4c02a6db24
wpioneer <a.shelepenok@gmail.com>
2016-04-12 18:30:26 +0300

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,21 @@
 $('.carousel').carousel();
 
-var middle_block_height = $('.middle-block').height();
-var left_sidebar_height = $('#left').height();
-var right_sidebar_height = $('#right').height();
+$(window).on('load resize', function() {
 
+    var middle_block_height = $('.middle-block').height();
+    var left_sidebar_height = $('#left').height();
+    var right_sidebar_height = $('#right').height();
+    var sidebar_width = $('.sidebar.sticky').width();
 
-
+    if ($(this).width() > 767) {
+        $('.sidebar').css('width', sidebar_width + 'px');
+        $('#left').css('height', middle_block_height + 'px');
+        $('#right').css('height', middle_block_height + 'px');
+    } else {
+        $('#left').css('height', 'auto');
+        $('#right').css('height', 'auto');
+    }
+});
 
 var stickyOptions = {
     disabled: false,
